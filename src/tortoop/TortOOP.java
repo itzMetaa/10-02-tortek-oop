866049ce5fc6097940e5a2380a2f2dfe52852ed3
Diak <Diak@10.4.117.8>
2019-10-02 16:22:04 +0200

    OraVege

--- src/tortoop/TortOOP.java
+++ src/tortoop/TortOOP.java
@@ -1,10 +1,13 @@
 package tortoop;
 
+import java.util.Random;
+
 public class TortOOP {
 
+    static Random r = new Random();
     public static void main(String[] args) {
-        Tort t = new Tort(4,8);
-        Tort t2 = new Tort(3,6);
+        Tort t = new Tort(r.nextInt(100)+1,r.nextInt(100)+1);
+        Tort t2 = new Tort(r.nextInt(100)+1,r.nextInt(100)+1);
         System.out.println(t);
         //t.kiir();
         t.legegyszerubbAlak();
