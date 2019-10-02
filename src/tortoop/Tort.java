8b09846f869122318c9baf364478f159feb0b46e
Diak <Diak@10.4.117.8>
2019-10-02 15:37:44 +0200

    tort class

---
+++ src/tortoop/Tort.java
@@ -0,0 +1,53 @@
+package tortoop;
+
+public class Tort {
+    public int szamlalo;
+    public int nevezo;
+    
+    public Tort(){}
+    public Tort(int szamlalo, int nevezo){
+        this.szamlalo = szamlalo;
+        this.nevezo = nevezo;
+    }
+    
+    public int getNevezo(){
+        return this.nevezo;
+    }
+    public int getSzamlalo(){
+        return this.szamlalo;
+    }
+    
+    
+    public void isEgszerusithetoE(){
+        
+    }
+    public void isEgeszE(){
+    
+    }
+    public void lnko(){
+    
+    }
+    public void lkkt(){
+    
+    }
+    public void osszeadas(){
+    
+    }
+    public void kivonas(){
+    
+    }
+    public void szorzas(){
+    
+    }
+    public void osztas(){
+    
+    }
+    public void kiir(){
+        System.out.println(this.szamlalo + "\n" +
+                "-\n" +
+                this.nevezo+ "\n");
+    }
+    
+    
+    
+}
--- src/tortoop/TortOOP.java
+++ src/tortoop/TortOOP.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tortoop;
 
-/**
- *
- * @author Diak
- */
 public class TortOOP {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Tort t = new Tort(1,2);
+        t.kiir();
+        t.kiir();
     }
     
 }
